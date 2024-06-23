67fdcdf1805ddae53690cdb43d5c527253deef36
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-06-23 04:34:15 +0300

    chore: Update SidebarButton and Feed components styling

--- src/screens/Feed.jsx
+++ src/screens/Feed.jsx
@@ -1,6 +1,6 @@
 const Feed = () => {
   return (
-    <div className="flex items-center justify-center text-white z-50 w-screen h-screen font-extrabold">
+    <div className="flex items-center justify-center text-white z-50 w-[calc(100%-100px)] h-screen font-extrabold">
       Feed
     </div>
   );
