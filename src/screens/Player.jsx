a23831ea243363205553d3e71ea3ef8d91c05a1b
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-03 20:09:54 +0300

    feat: Add Player component with useEffect to fetch playlist tracks

--- src/screens/Player.jsx
+++ src/screens/Player.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { apiClient } from "../../spotify";
 
 const Player = () => {
-  return <div>Player</div>;
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state) {
+      apiClient.get(`/playlists/${location.state?.id}/tracks`).then((res) => {
+        console.log(res.data);
+      });
+    }
+  }, [location.state]);
+
+  return (
+    <div className="w-[calc(100%-100px)] h-screen overflow-x-hidden bg-[#1e2a3e] rounded-3xl bg-custom-gradient">
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Player;
