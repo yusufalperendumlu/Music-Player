765077092fa87bbdf8904fcda5ecc542c8dd1bb4
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-07 21:45:09 +0300

    feat: Improve AlbumImage and AlbumInfo components in SongCard

--- src/components/album/AlbumImage.jsx
+++ src/components/album/AlbumImage.jsx
@@ -1,6 +1,12 @@
 const AlbumImage = ({ url }) => {
-  console.log(url);
-  return <div>AlbumImage</div>;
+  return (
+    <div className="w-4/5 flex items-center justify-center relative z-50">
+      <img src={url} alt="album" className="rounded-3xl w-full aspect-square" />
+      <div className="w-[90%] absolute -z-10 top-16 blur-lg">
+        <img src={url} alt="album" className="w-full h-3/4 rounded-t-3xl" />
+      </div>
+    </div>
+  );
 };
 
 export default AlbumImage;
--- src/components/album/AlbumInfo.jsx
+++ src/components/album/AlbumInfo.jsx
@@ -1,5 +1,18 @@
-const AlbumInfo = () => {
-  return <div>AlbumInfo</div>;
+const AlbumInfo = ({ album }) => {
+  console.log(album);
+  return (
+    <div className="">
+      <div>
+        <p>{album?.name}</p>
+      </div>
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default AlbumInfo;
