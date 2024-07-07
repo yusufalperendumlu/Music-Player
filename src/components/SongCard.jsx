df71bddea6e92fc388af663d0b0bbc9cc47c3bef
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-07 21:45:00 +0300

    refactor: Add flex layout to SongCard for better alignment

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,7 +3,7 @@ import AlbumInfo from "@/components/album/AlbumInfo";
 
 const SongCard = ({ album }) => {
   return (
-    <div className="w-full h-2/3 bg-[#27354d] rounded-3xl rounded-br-none">
+    <div className="w-full h-2/3 bg-[#27354d] rounded-3xl rounded-br-none flex flex-col items-center justify-center">
       <AlbumImage url={album?.images[0]?.url} />
       <AlbumInfo album={album} />
     </div>
