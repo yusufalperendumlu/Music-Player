68aa205a904c9063efe8913c99f486062593c038
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-08 22:25:55 +0300

    feat: Add custom gradient styles and animation to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,26 @@ export default {
       backgroundImage: {
         "custom-gradient":
           "repeating-radial-gradient(circle at 0 0, transparent 0, #233656 10px), repeating-linear-gradient(#1e2a3e55, #1e2a3e)",
+        "custom-gradient-b":
+          "repeating-radial-gradient(circle at 0 0, transparent 0, #3e61d2 40px ), repeating-linear-gradient(#4767d055, #4767d0);",
       },
       backgroundColor: {
         "custom-linear-gradient":
           "linear-gradient(75deg, rgb(40, 58, 88) 0%, rgba(54, 69, 98, 0) 100%)",
         "custom-linear-g":
           "linear-gradient(180deg, rgba(54, 69, 98, 0), 10% rgba(54, 69, 98, 1) 100%)",
+      },
+      animation: {
+        marquee: "marquee 12s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translate(0, 0)" },
+          "100%": { transform: "translate(-100%, 0)" },
+        },
+      },
+      transformOrigin: {
+        half: "50% 50%",
       },
     },
   },
