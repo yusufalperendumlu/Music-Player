433d93b0de5c41ebec338ef1b24c319b18f68074
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-08 22:26:15 +0300

    feat: Update AlbumImage component styling and positioning

--- src/components/album/AlbumImage.jsx
+++ src/components/album/AlbumImage.jsx
@@ -1,8 +1,8 @@
 const AlbumImage = ({ url }) => {
   return (
-    <div className="w-4/5 flex items-center justify-center relative z-50">
+    <div className="w-3/5 flex items-center justify-center relative z-50">
       <img src={url} alt="album" className="rounded-3xl w-full aspect-square" />
-      <div className="w-[90%] absolute -z-10 top-16 blur-lg">
+      <div className="w-[90%] absolute -z-50 top-12 blur-lg">
         <img src={url} alt="album" className="w-full h-3/4 rounded-t-3xl" />
       </div>
     </div>
