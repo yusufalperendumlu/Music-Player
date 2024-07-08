e9ba4865b052f97f39f31a67d672d6e79e864b55
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-08 22:26:09 +0300

    feat: Update SongCard component with improved styling and lazy loading for album images

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -2,9 +2,11 @@ import AlbumImage from "@/components/album/AlbumImage";
 import AlbumInfo from "@/components/album/AlbumInfo";
 
 const SongCard = ({ album }) => {
+  console.log(album);
+
   return (
-    <div className="w-full h-2/3 bg-[#27354d] rounded-3xl rounded-br-none flex flex-col items-center justify-center">
-      <AlbumImage url={album?.images[0]?.url} />
+    <div className="w-full h-auto py-8 bg-[#27354d] rounded-3xl rounded-br-none flex flex-col items-center justify-center">
+      <AlbumImage url={album?.album?.images[0]?.url} />
       <AlbumInfo album={album} />
     </div>
   );
