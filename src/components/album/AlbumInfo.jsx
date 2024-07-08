d1ae1cc09c17ad56519a215e5bd102016a58d9af
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-08 22:26:29 +0300

    feat: Update AlbumInfo component to display album name, artists, and release date

--- src/components/album/AlbumInfo.jsx
+++ src/components/album/AlbumInfo.jsx
@@ -1,15 +1,31 @@
 const AlbumInfo = ({ album }) => {
   console.log(album);
+
+  const artists = [];
+
+  album?.artists?.forEach((e) => {
+    artists.push(e.name);
+  });
+
   return (
-    <div className="">
-      <div>
-        <p>{album?.name}</p>
+    <div className="mt-5 w-3/5">
+      <div className="w-full text-xl overflow-hidden font-bold text-white">
+        <div className="whitespace-nowrap inline-block animate-marquee pl-[100%]">
+          <p className="mt-1">
+            {album?.name} - {artists?.join(", ")}{" "}
+          </p>
+        </div>
       </div>
-      <div>
-        <p></p>
+      {/* <div className="text-sm font-medium text-slate-400">
+        <p className="mt-1">{artists?.join(", ")} </p>
+      </div> */}
+      <div className="text-sm font-semibold text-slate-400 overflow-hidden line-clamp-1 hover:line-clamp-none cursor-pointer">
+        <p className="mt-1">{`${album?.album?.name} is an ${
+          album?.album_type
+        } by ${artists?.join(", ")} with ${album?.total_tracks} track(s)`}</p>
       </div>
-      <div>
-        <p></p>
+      <div className="text-xs font-normal text-slate-400">
+        <p className="mt-2">Release Date: {`${album?.release_date}`}</p>
       </div>
     </div>
   );
