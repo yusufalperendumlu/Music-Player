84f328d9264c4b1d5e090618ab368aa411609abc
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-08 22:26:32 +0300

    feat: Add lazy loading for album images in SongCard component

--- src/screens/Player.jsx
+++ src/screens/Player.jsx
@@ -2,33 +2,41 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { apiClient } from "../../spotify";
 
+import AudioPlayer from "@/components/audioPlayer/AudioPlayer";
 import SongCard from "@/components/SongCard";
 import Queue from "@/components/Queue";
 
 const Player = () => {
   const location = useLocation();
-  const [track, setTrack] = useState([]);
-  const [currentTrack, setCurrentTruck] = useState({});
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(currentTrack);
+  console.log(tracks);
 
   useEffect(() => {
     if (location.state) {
       apiClient.get(`/playlists/${location.state?.id}/tracks`).then((res) => {
-        setTrack(res.data.items);
-        setCurrentTruck(res.data.items[0].track);
-        console.log(res.data.items);
+        setTracks(res?.data?.items);
+        setCurrentTrack(res?.data?.items[0]?.track);
       });
     }
   }, [location.state]);
 
+  useEffect(() => {
+    setCurrentTrack(tracks[currentIndex]?.track);
+  }, [currentIndex, tracks]);
+
+  console.log(currentTrack);
+
   return (
     <div className="w-[calc(100%-100px)] h-screen flex  overflow-x-hidden bg-[#1e2a3e] rounded-3xl bg-custom-gradient">
-      <div className="w-4/6 h-full mr-4"></div>
-      <div className="w-1/3 h-full flex flex-col justify-between">
-        <SongCard album={currentTrack.album} />
-        <Queue />
+      <div className="w-4/6 h-full mr-4">
+        <AudioPlayer currentTrack={currentTrack} />
+      </div>
+      <div className="w-1/3 h-full flex flex-col justify-between fixed right-0 overflow-y-scroll">
+        <SongCard album={currentTrack} />
+        <Queue tracks={tracks} setCurrentIndex={setCurrentIndex} />
       </div>
     </div>
   );
