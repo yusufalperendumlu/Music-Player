4e6da531e1a0db90ccd8a4ea96c68273f5ff27b1
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-10 20:51:37 +0300

    feat: Create Controls component for audio player

---
+++ src/components/audioPlayer/Controls.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Controls = ({
+  isPlaying,
+  setIsPlaying,
+  handleNext,
+  handlePrev,
+  total,
+}) => {
+  return <div>Controls</div>;
+};
+
+export default Controls;
