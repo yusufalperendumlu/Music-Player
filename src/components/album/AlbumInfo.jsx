1312525c3e03540a6b20dab059418785ff67d5d6
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-11 19:13:44 +0300

    refactor: Improve album information display in AlbumInfo component

--- src/components/album/AlbumInfo.jsx
+++ src/components/album/AlbumInfo.jsx
@@ -9,7 +9,7 @@ const AlbumInfo = ({ album }) => {
   return (
     <div className="mt-5 w-3/5">
       <div className="w-full text-xl overflow-hidden font-bold text-white">
-        <div className="whitespace-nowrap inline-block animate-marquee pl-[100%]">
+        <div className="whitespace-nowrap inline-block transition duration-500 ease-linear hover:animate-marquee ">
           <p className="mt-1">
             {album?.name} - {artists?.join(", ")}{" "}
           </p>
@@ -18,10 +18,14 @@ const AlbumInfo = ({ album }) => {
       {/* <div className="text-sm font-medium text-slate-400">
         <p className="mt-1">{artists?.join(", ")} </p>
       </div> */}
-      <div className="text-sm font-semibold text-slate-400 overflow-hidden line-clamp-1 hover:line-clamp-none cursor-pointer">
-        <p className="mt-1">{`${album?.album?.name} is an ${
-          album?.album?.album_type
-        } by ${artists?.join(", ")} with ${album?.total_tracks} track(s)`}</p>
+      <div className="overflow-x-hidden whitespace-nowrap w-full">
+        <div className="text-sm font-semibold text-slate-400 line-clamp-1 hover:line-clamp-none flex text-nowrap hover:animate-marquee cursor-pointer">
+          <p className="mt-1">{`${album?.album?.name} is an ${
+            album?.album?.album_type
+          } by ${artists?.join(", ")} with ${
+            album?.album?.total_tracks
+          } track(s)`}</p>
+        </div>
       </div>
       <div className="text-xs font-normal text-slate-400">
         <p className="mt-2">Release Date: {`${album?.release_date}`}</p>
