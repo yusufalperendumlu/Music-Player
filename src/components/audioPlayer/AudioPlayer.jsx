60c31e50872a8cfbc61e7d9df7a625970993ca82
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-07-16 20:08:05 +0300

    refactor: Update AudioPlayer component to include track progress and controls

--- src/components/audioPlayer/AudioPlayer.jsx
+++ src/components/audioPlayer/AudioPlayer.jsx
@@ -1,7 +1,7 @@
 import Controls from "@/components/audioPlayer/Controls";
 import ProgressCircle from "@/components/audioPlayer/ProgressCircle";
 import WaveAnimation from "@/components/audioPlayer/WaveAnimation";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AudioPlayer = ({
   currentTrack,
@@ -23,6 +23,79 @@ const AudioPlayer = ({
   const { duration } = audioRef.current;
 
   const currentPercentage = duration ? (trackProgress / duration) * 100 : 0;
+
+  console.log(audioRef);
+
+  const stratTimer = () => {
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        handleNext();
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, [1000]);
+  };
+
+  useEffect(() => {
+    if (audioRef.current.src) {
+      if (isPlaying) {
+        audioRef.current.play();
+        stratTimer();
+      } else {
+        clearInterval(intervalRef.current);
+        audioRef.current.pause();
+      }
+    } else {
+      if (isPlaying) {
+        audioRef.current = new Audio(audioSrc);
+        audioRef.current.play();
+        stratTimer();
+      } else {
+        clearInterval(intervalRef.current);
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying, audioSrc]);
+
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(audioSrc);
+
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      stratTimer();
+    } else {
+      isReady.current = true;
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  const handleNext = () => {
+    if (currentIndex === total.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex - 1 < 0) {
+      setCurrentIndex(total.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
 
   const artists = [];
   currentTrack?.artists?.forEach((e) => {
@@ -51,7 +124,7 @@ const AudioPlayer = ({
         <div className="flex items-center w-full flex-col">
           <div className="w-1/2 flex justify-between mb-5">
             <p className="text-xl flex items-center font-bold m-0 text-[#c4d0e3]">
-              0:00
+              {new Date(trackProgress * 1000).toISOString().substring(14, 19)}
             </p>
             <WaveAnimation isPlaying={isPlaying} />
             <p className="text-xl flex items-center font-bold m-0 text-[#c4d0e3]">
@@ -63,9 +136,9 @@ const AudioPlayer = ({
             </p>
           </div>
           <Controls
-            // handleNext={setTrackProgress}
-            // handlePrev={setTrackProgress}
-            isPlaying={setIsPlaying}
+            handleNext={handleNext}
+            handlePrev={handlePrev}
+            isPlaying={isPlaying}
             setIsPlaying={setIsPlaying}
             total={total}
           />
